1c598dd7d2139ba20c4c1324a772397035e06ded
FroomeBeibei79 <xh231@georgetown.edu>
2023-09-28 17:36:07 -0400

    working on data

---
+++ Data Cleaning.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import re
+import pandas as pd
+import csv
+from sklearn.feature_extraction.text import CountVectorizer
+
+api_url = "https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseDetails?stateCase=10003&caseYear=2014&state=1&format=json
+
+response = reques
